b808810849abf8fd5eae681d8daf6842dd32dcb3
adisaktijrs <adhisakti47@gmail.com>
2024-04-24 16:48:48 +0700

    feat: add cons example of csr

--- packages/csr/src/App.tsx
+++ packages/csr/src/App.tsx
@@ -1,10 +1,19 @@
+import useSWR from "swr";
 import "./App.css";
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 function App() {
+  const { data, isLoading } = useSWR<{ data: string }>(
+    `http://localhost:5000/api?key=${import.meta.env.VITE_API_KEY}`,
+    fetcher
+  );
+
   return (
     <>
       <h1 className="text-3xl text-blue-600">Hello world!</h1>
       <p>This is an example of react app with CSR rendering strategy</p>
+      <p className="text-red-800">{isLoading ? "loading..." : data?.data}</p>
     </>
   );
 }
--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World';
+  }
+
+  async getDataWithKey(key: string) {
+    await this.delay(2000);
+
+    // so strong!
+    if (key !== 'rahasianegara') {
+      throw new UnauthorizedException();
+    }
+    return {
+      message: 'dummy api with api key',
+      data: 'the secret data you could get when you have a key.',
+    };
+  }
+
+  async delay(time: number) {
+    return await new Promise((res) => setTimeout(() => res(''), time));
   }
 }
